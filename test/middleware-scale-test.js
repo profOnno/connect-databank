e77419f4c05a2e79df41a4e1a6b73158e57e9c2e
profOnno <menno.vossen@kuopzi.nl>
2015-11-28 13:13:11 +0100

    indent cleanup

--- test/middleware-scale-test.js
+++ test/middleware-scale-test.js
@@ -27,7 +27,7 @@ var _ = require("underscore"),
     Logger = require("bunyan"),
     connect = require("connect"),
     session = require("express-session"),
-    cookieParser = require("cookie-parser"),
+//    cookieParser = require("cookie-parser"),
     Browser = require("zombie"),
     Databank = databank.Databank;
 
@@ -50,71 +50,71 @@ suite.addBatch({
             },
             "and we instantiate a store": {
                 topic: function(DatabankStore) {
-		    var callback = this.callback,
-		        db = Databank.get("memory", {});
+                    var callback = this.callback,
+                    db = Databank.get("memory", {});
 
-		    db.connect({}, function(err) {
-			var store;
-			if (err) {
-			    callback(err, null);
-			} else {
-			    try {
-				store = new DatabankStore(db);
-				callback(null, store, db);
-			    } catch (e) {
-				callback(e, null, null);
-			    }
-			}
-		    });
-		},
+                    db.connect({}, function(err) {
+                        var store;
+
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            try {
+                            store = new DatabankStore(db);
+                            callback(null, store, db);
+                            } catch (e) {
+                            callback(e, null, null);
+                            }
+                        }
+                    });
+                },
                 teardown: function(store, db) {
-		    if (db && db.disconnect) {
-			db.disconnect(function(err) {});
-		    }
-		},
+                    if (db && db.disconnect) {
+                        db.disconnect(function(err) {});
+                    }
+                },
                 "it works": function(err, store, db) {
                     assert.ifError(err);
                     assert.isObject(store);
                     assert.isObject(db);
                 },
-		"and we start an app using the store": {
-		    topic: function(store) {
-			var cb = this.callback,
-			    app = connect();
+                "and we start an app using the store": {
+                    topic: function(store) {
+                        var cb = this.callback,
+                            app = connect();
 
-			app.use(cookieParser());
-			app.use(session({secret: "test", store: store}));
+                        //app.use(cookieParser());
+                        app.use(session({secret: "test", saveUninitialized: true, resave: false, store: store}));
 
-			app.use(function(req, res) {
-			    var cb;
-			    req.session.lastUrl = req.originalUrl;
-			    if (req.session.hits) {
-				req.session.hits++;
-			    } else {
-				req.session.hits = 1;
-			    }
-			    res.end("Hello, world!");
-			    // Leak the session out the side door
-			    if (app.callback) {
-				cb = app.callback;
-				process.nextTick(function() {
-				    cb(null, req.session);
-				});
-			    }
-			});
+                        app.use(function(req, res) {
+                            var cb;
+                            req.session.lastUrl = req.originalUrl;
+                            if (req.session.hits) {
+                                req.session.hits++;
+                            } else {
+                                req.session.hits = 1;
+                            }
+                            res.end("Hello, world!");
+                            // Leak the session out the side door
+                            if (app.callback) {
+                                cb = app.callback;
+                                process.nextTick(function() {
+                                    cb(null, req.session);
+                                });
+                            }
+                        });
             
-            console.log("hellow");
+                        console.log("hellow");
 
-			app.listen(1516, function() {
-			    cb(null, app);
-			});
-		    },
-		    "it works": function(err, app) {
-			assert.ifError(err);
-			assert.ok(app);
-		    },
-		    "and we browse around the app with a few browsers": {
-
+                        app.listen(1516, function() {
+                            cb(null, app);
+                        });
+                    },
+                    "it works": function(err, app) {
+                        assert.ifError(err);
+                        assert.ok(app);
+                    },
+                    "and we browse around the app with a few browsers": {
                         topic: function(app, store) {
 
                             var callback = this.callback,
@@ -136,45 +136,45 @@ suite.addBatch({
                                 wanderAround = function(br, id, pagesLeft, callback) {
                                     var p = Math.floor(Math.random() * MAXPAGE),
                                         oldSid = sidOf(br);
-				    br.visit("http://localhost:1516/"+p, function(err) {
-					if (err) {
-                                            callback(err, null);
-                                        } else if (pagesLeft == 1) {
-                                            lasts[id] = p;
-                                            callback(null, br);
-                                        } else if (oldSid && sidOf(br) != oldSid) {
-                                            callback(new Error("SID of browser changed from " + oldSid + " to " + sidOf(br)));
-                                        } else {
-                                            wanderAround(br, id, pagesLeft - 1, callback);
-                                        }
-				    });
-                                };
+                                        br.visit("http://localhost:1516/"+p, function(err) {
+                                            if (err) {
+                                                callback(err, null);
+                                            } else if (pagesLeft == 1) {
+                                                lasts[id] = p;
+                                                callback(null, br);
+                                            } else if (oldSid && sidOf(br) != oldSid) {
+                                                callback(new Error("SID of browser changed from " + oldSid + " to " + sidOf(br)));
+                                            } else {
+                                                wanderAround(br, id, pagesLeft - 1, callback);
+                                            }
+                                        });
+                                    };
 
-                            Step(
-                                function() {
-                                    var group = this.group(), i;
-                                    for (i = 0; i < MAXBROWSERS; i++) {
-                                        counts[i] = 10 + Math.floor(Math.random()*20);
-                                        wanderAround(new Browser(), i, counts[i], group());
+                                Step(
+                                    function() {
+                                        var group = this.group(), i;
+                                        for (i = 0; i < MAXBROWSERS; i++) {
+                                            counts[i] = 10 + Math.floor(Math.random()*20);
+                                            wanderAround(new Browser(), i, counts[i], group());
+                                        }
+                                    },
+                                    function(err, browsers, ps) {
+                                        var group = this.group();
+                                        if (err) throw err;
+                                        _.each(browsers, function(br) {
+                                            var sid = sidOf(br);
+                                            store.get(sid, group());
+                                        });
+                                    },
+                                    function(err, sessions) {
+                                        if (err) {
+                                            callback(err);
+                                        } else {
+                                            callback(err, lasts, counts, sessions);
+                                        }
                                     }
-                                },
-                                function(err, browsers, ps) {
-                                    var group = this.group();
-                                    if (err) throw err;
-                                    _.each(browsers, function(br) {
-                                        var sid = sidOf(br);
-                                        store.get(sid, group());
-                                    });
-                                },
-                                function(err, sessions) {
-                                    if (err) {
-                                        callback(err);
-                                    } else {
-                                        callback(err, lasts, counts, sessions);
-                                    }
-                                }
-                            );
-			},
+                                );
+                        },
                         "it works": function(err, lasts, counts, sessions) {
                             assert.ifError(err);
                         },
@@ -186,9 +186,9 @@ suite.addBatch({
                                 assert.equal(sessions[i].lastUrl, "/"+lasts[i]);
                             }
                         }
-		    }
-	        }
-	    }
+                    }
+                }
+            }
         }
     }
 });
